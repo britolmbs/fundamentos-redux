5135c75d68ab4c186612a2c3493854fdcf6ad0eb
Lucas Brito <britolmbs@hotmail.com>
2025-01-11 16:35:06 -0300

    Componente Card

--- fundamentos-redux/src/App.js
+++ fundamentos-redux/src/App.js
@@ -7,7 +7,12 @@ function App() {
     <div className="App">
       <h1>Exercicio React-Redux(Simples)</h1>
       <div className='linha'>
-      <Card />
+      <Card title="Card 1" red />
+      <Card title="Card 2" green />
+      </div>
+      <div className='linha'>
+      <Card title="Card 3" blue />
+      <Card title="Card 4" purple />
       </div>
      
     </div>
