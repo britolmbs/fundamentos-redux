08c2aa859a63e87d2b613b4a1f59a36a3d0253fd
Lucas Brito <britolmbs@hotmail.com>
2025-01-12 16:24:34 -0300

    Componente intervalo

--- fundamentos-redux/src/App.js
+++ fundamentos-redux/src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import Intervalo from './components/intervalo';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercicio React-Redux(Simples)</h1>
       <div className='linha'>
-      <Card title="Card 1" red />
-      <Card title="Card 2" green />
+      <Intervalo></Intervalo>
       </div>
       <div className='linha'>
+      <Card title="Card 2" green />
       <Card title="Card 3" blue />
       <Card title="Card 4" purple />
       </div>
