ed962bd246fd99d12a53aaaf0d5bab8ec351205d
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-01-14 10:50:16 -0300

    Config redux

--- fundamentos-redux/src/store/storeConfig.js
+++ fundamentos-redux/src/store/storeConfig.js
@@ -1,5 +1,22 @@
 import { createStore, combineReducers } from 'redux'
 
+<<<<<<< HEAD
+const reducers = combineReducers({
+    numeros: function(state, action){
+        return{
+            min: 1,
+            max: 10
+        }
+    },
+    
+})
+
+function storeConfig() {
+    return createStore (reducers)
+}
+
+export default storeConfig
+=======
 const reducers = combineReducers({
     numeros: function(state, action){
         return{
@@ -15,3 +32,4 @@ function storeConfig() {
 }
 
 export default
+>>>>>>> ff882add58e35c0fbbb716e5c992bddbaaac2b7a
