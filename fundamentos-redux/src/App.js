ff882add58e35c0fbbb716e5c992bddbaaac2b7a
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-01-14 10:41:50 -0300

    Configurando o reducer

--- fundamentos-redux/src/App.js
+++ fundamentos-redux/src/App.js
@@ -8,21 +8,18 @@ import { useState } from 'react';
 
 function App() {
 
-  const [min, setMin] = useState(1)
-  const [max, setMax] = useState(10)
+
   
   return (
     <div className="App">
       <h1>Exercicio React-Redux(Simples)</h1>
       <div className='linha'>
-      <Intervalo min={min} max={max}
-      onMinChanged={setMin} onMaxChanged={setMax}
-      ></Intervalo>
+      <Intervalo></Intervalo>
       </div>
       <div className='linha'>
-      <Media min={min} max={max}></Media>
-      <Soma min={min} max={max}></Soma>
-      <Sorteio min={min} max={max}></Sorteio>
+      <Media></Media>
+      <Soma></Soma>
+      <Sorteio></Sorteio>
       </div>
      
     </div>
